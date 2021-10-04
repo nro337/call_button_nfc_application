da121f38012cdc4c6aea27952699fa9f27cece01
nro337 <nro337@gmail.com>
2021-10-04 13:12:38 -0400

    Read/Write MVP

--- App.js
+++ App.js
@@ -1,12 +1,83 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import NfcManager, {NfcTech, Ndef, NfcEvents} from 'react-native-nfc-manager';
+
+// Pre-step, call this before any NFC operations
+async function initNfc() {
+  await NfcManager.start();
+}
+
+async function writeNdef({type, value}) {
+  let result = false;
+
+  try {
+    // Step 1
+    await NfcManager.requestTechnology(NfcTech.Ndef, {
+      alertMessage: 'Ready to write some NDEF',
+    });
+
+    const bytes = Ndef.encodeMessage([Ndef.textRecord('Hello NFC')]);
+
+    if (bytes) {
+      await NfcManager.ndefHandler // Step2
+        .writeNdefMessage(bytes); // Step3
+
+      if (Platform.OS === 'ios') {
+        await NfcManager.setAlertMessageIOS('Successfully write NDEF');
+      }
+    }
+
+    result = true;
+  } catch (ex) {
+    console.warn(ex);
+  }
+
+  // Step 4
+  NfcManager.cancelTechnologyRequest().catch(() => 0);
+  return result;
+}
+
+function readNdef() {
+  const cleanUp = () => {
+    NfcManager.setEventListener(NfcEvents.DiscoverTag, null);
+    NfcManager.setEventListener(NfcEvents.SessionClosed, null);
+  };
+
+  return new Promise((resolve) => {
+    let tagFound = null;
+
+    NfcManager.setEventListener(NfcEvents.DiscoverTag, (tag) => {
+      tagFound = tag;
+      resolve(tagFound);
+      NfcManager.setAlertMessageIOS('NDEF tag found');
+      console.log(NfcManager.ndefHandler.getNdefMessage(Ndef.decodeMessage([Ndef.text.decodePayload([tag])])));
+      NfcManager.unregisterTagEvent().catch(() => 0);
+    });
+
+    NfcManager.setEventListener(NfcEvents.SessionClosed, () => {
+      cleanUp();
+      if (!tagFound) {
+        resolve();
+      }
+    });
+
+    NfcManager.registerTagEvent();
+  });
+}
 
 export default function App() {
+  useEffect(() => {
+    initNfc();
+  })
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>NFC Read/Write MVP</Text>
       <StatusBar style="auto" />
+      <Button title="Write" onPress={writeNdef}></Button>
+      <Button title="Read" onPress={readNdef}></Button>
     </View>
   );
 }
